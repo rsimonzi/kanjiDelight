ae777677915da998cea0fce310afefb72fbd9d2e
rsimonzi <rsimonzi@hotmail.com>
2021-02-27 22:31:48 -0800

    more grammar points

--- app.js
+++ app.js
@@ -423,7 +423,7 @@ const lesson6 = new Lesson({
   grammarPoints: [
     "Japanese surnames often consist of two or more characters.",
     "Adjectives that are used with な are called 形容動詞",
-    "",
+    "Honorific speech is used to refer to third parties as well as the listener",
     "",
     "",
   ],
